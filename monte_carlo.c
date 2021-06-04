99c19a9d6345782ac205423fdf775a8aaae917a4
sudha-7 <84711015+sudha-7@users.noreply.github.com>
2021-06-04 23:07:17 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,7 +3,17 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
+float mc_pi(int){
+  int circle=0;
+  float x,y;
+  for(int i=1;i<=n;i++)
+  {
+    x=frandom();
+    y=frandom();
+    if(x*x + y*y <= 1){circle++;}
+  }
+  return 4*((float)circle/n);
+}
 
 float frandom() {
   long int q = random();
